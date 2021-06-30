2a6af57d48d4a6f693d9662cd70649239d9956b2
brickbox_dev <dev@brickbox.io>
2021-06-30 16:24:01 -0400

    wip: Linting

--- bb_api/views.py
+++ bb_api/views.py
@@ -3,7 +3,7 @@
 # from django.contrib.auth.models import User, Group
 
 from rest_framework import viewsets
-from rest_framework import permissions
+# from rest_framework import permissions
 
 from bb_api.serializers import (
     # UserSerializer, GroupSerializer,
