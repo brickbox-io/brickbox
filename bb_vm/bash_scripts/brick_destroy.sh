26b5ddc5663eb8ee66a8c432c950fee291095069
Justin Merrell <merrelljustin@gmail.com>
2021-11-11 15:47:23 +0000

    fix: Bash fix

--- bb_vm/bash_scripts/brick_destroy.sh
+++ bb_vm/bash_scripts/brick_destroy.sh
@@ -21,7 +21,7 @@ sleep 5
 sudo rm /var/lib/libvirt/images/"$instance".img 2>> bash_errors.log
 
 # Confirm that the image is deleted
-if [ sudo -f /var/lib/libvirt/images/"$instance".img ]; then
+if [ -f /var/lib/libvirt/images/"$instance".img ]; then
     echo "Image not deleted"
     curl -X POST https://"$url"/api/vmlog/ -d "level=50&virt_brick=$instance&message=Brick%20was%20not%20destroyed." &
 
