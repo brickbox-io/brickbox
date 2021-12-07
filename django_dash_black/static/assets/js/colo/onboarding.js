35ad7cd1a89866651cec5cec9e7ce83e6d1ed4c4
Justin Merrell <merrelljustin@gmail.com>
2021-12-07 17:40:53 +0000

    fix: JS Linter Fix

--- django_dash_black/static/assets/js/colo/onboarding.js
+++ django_dash_black/static/assets/js/colo/onboarding.js
@@ -14,8 +14,7 @@ function TermsAgreement() {
 
         xhttp.onload = function () {
             var vm_status = JSON.parse(this.responseText);
-            BrickNotice('bottom', 'center', 'Brick Crumbled');
-            document.getElementById("brick_wall").innerHTML = vm_status.table;
+            BrickNotice('bottom', 'center', 'Terms Signed');
         }
 
         xhttp.open('POST', url, true);
@@ -28,8 +27,24 @@ function TermsAgreement() {
 /* ------------------------------- ETH Address ------------------------------ */
 function ETH_Address() {
     $("#eth_address").modal('toggle');
+
     $("#confirm_button").click(function () {
         $("#eth_address").modal('toggle');
+        var formData = new FormData();
+        var xhttp = new XMLHttpRequest();
+        var url = '/colo/eth_address/'
 
+        user_id = document.getElementById("confirm_button").dataset.client;
+
+        formData.append('user_id', user_id);
+
+        xhttp.onload = function () {
+            var vm_status = JSON.parse(this.responseText);
+            BrickNotice('bottom', 'center', 'ETH Address Added');
+        }
+
+        xhttp.open('POST', url, true);
+        xhttp.setRequestHeader("X-CSRFToken", csrftoken);
+        xhttp.send(formData);
     });
 }
