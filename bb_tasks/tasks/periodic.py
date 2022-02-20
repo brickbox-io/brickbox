a2326eed5816b87f75002aa7783891cd96556218
Justin Merrell <merrelljustin@gmail.com>
2022-02-20 16:37:32 +0000

    refactor: Linting

--- bb_tasks/tasks/periodic.py
+++ bb_tasks/tasks/periodic.py
@@ -2,7 +2,7 @@
 
 from __future__ import absolute_import, unicode_literals
 
-import subprocess
+# import subprocess
 from subprocess import Popen, PIPE
 import datetime
 from dateutil.relativedelta import relativedelta
