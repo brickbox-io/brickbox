3760592d9a9f1b89953d8bfcecc0b6b5aa12c058
Justin Merrell <merrelljustin@gmail.com>
2022-03-28 23:15:16 +0000

    refactor: 404 Page

--- bb_dashboard/views/views.py
+++ bb_dashboard/views/views.py
@@ -89,7 +89,7 @@ def pages(request):
 
     except template.TemplateDoesNotExist:
 
-        html_template = loader.get_template( 'page-404.html' )
+        html_template = loader.get_template( '404.html' )
         return HttpResponse(html_template.render(context, request))
 
     # except:
