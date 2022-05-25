a74a31449372c1b487ed1b3164ed7a2a553d91bc
Justin Merrell <merrelljustin@gmail.com>
2022-05-25 16:59:04 +0000

    test(bb_vm): URL Tests

--- bb_vm/tests/tests_url.py
+++ bb_vm/tests/tests_url.py
@@ -16,4 +16,20 @@ class TestUrls(TestCase):
         '''
         # Host Onboard
         url_host_onboard = reverse('bb_vm:host_onboarding', kwargs={'host_serial': '1234567890'})
-        self.assertEqual(url_host_onboard, 'vm/host/oboarding/<host_serial>/')
+        self.assertEqual(url_host_onboard, 'vm/host/oboarding/1234567890/')
+
+        # Host Onboard Public Key
+        url_host_onboard_pubkey = reverse('bb_vm:host_onboarding_pubkey', kwargs={'host_serial': '1234567890'})
+        self.assertEqual(url_host_onboard_pubkey, 'vm/host/oboarding/pubkey/1234567890/')
+
+    def test_resolve_urls(self):
+        '''
+        Test that the correct view is used for the URL request.
+        '''
+        # Host Onboard
+        resolve_host_onboard = resolve('vm/host/oboarding/1234567890/')
+        self.assertEqual(resolve_host_onboard.func.__name__, 'host_onboarding')
+        self.assertEqual(resolve_host_onboard.args, ())
+        self.assertEqual(resolve_host_onboard.kwargs['host_serial'], '1234567890')
+        self.assertEqual(resolve_host_onboard.url_name, 'host_onboarding')
+        self.assertEqual(resolve_host_onboard.view_name, 'bb_vm:host_onboarding')
